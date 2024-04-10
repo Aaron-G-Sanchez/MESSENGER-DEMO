5073c428c658b1d767e11362292d18e681a34bcb
Aaron Sanchez <saarong12@gmail.com>
2024-04-10 00:01:54 -0500

    Added test to validate the association between user and message tables.

--- __tests__/model.test.js
+++ __tests__/model.test.js
@@ -10,8 +10,8 @@ describe('Models', () => {
   describe('User', () => {
     test('Can create a user', async () => {
       const newUser = {
-        userId: 123456,
-        userName: 'Chravis'
+        userId: 123,
+        userName: 'User 123'
       }
 
       const createdUser = await User.create(newUser)
@@ -31,6 +31,35 @@ describe('Models', () => {
 
       expect(createMessage).toBeInstanceOf(Message)
       expect(createMessage).toEqual(expect.objectContaining(testMessage))
+    })
+  })
+
+  describe('Associations', () => {
+    test('Should associate a message to a user', async () => {
+      const userWithAssociation = {
+        userId: 456,
+        userName: 'User 456'
+      }
+
+      const messageWithAssociation = {
+        message: 'Message for user 456'
+      }
+
+      const newUser = await User.create(userWithAssociation)
+      const newMessage = await Message.create(messageWithAssociation)
+
+      await newUser.addMessage(newMessage)
+
+      const userWithMessage = await User.findByPk(newUser.id, {
+        include: Message
+      })
+
+      expect(userWithMessage).toBeInstanceOf(User)
+      expect(userWithMessage.Messages).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining(messageWithAssociation)
+        ])
+      )
     })
   })
 })
