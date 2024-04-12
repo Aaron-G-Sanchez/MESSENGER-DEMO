8e79f6237f67d3490336caed96630fbe248c6fc8
Aaron Sanchez <saarong12@gmail.com>
2024-04-12 01:08:40 -0500

    Added todos to the messages route

--- routes/app.js
+++ routes/app.js
@@ -5,12 +5,15 @@ const { getMessages } = require('../controllers')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Test route. WIll delete
 app.get('/', (req, res, next) => {
   res.send({ msg: 'Hello World' })
 })
 
+// TODO Need to move route to sepereate file
 app.get('/messages/:userId/:chatId', async (req, res, next) => {
   const { userId, chatId } = req.params
+  // TODO Add logic to check if messages come back empty
   let [messages, error] = await getMessages(userId, chatId)
   if (error != undefined) {
     return next(error)
